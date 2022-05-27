006ecea92deca780e7c036a7879d15d6e75cf9cb
muralitharan805 <muralitharan805@gmail.com>
2022-05-27 12:21:45 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,17 +2,27 @@
 import './style.css';
 
 // Write Javascript code!
-var store = '';
+var store = [];
 
 function test() {
   // console.log('hello');
   var getTextbox = document.getElementById('nameText');
   console.log(getTextbox.value);
 
-  var getDisplay = document.getElementById('display');
-  getDisplay.innerHTML = getTextbox.value;
+  store.push(getTextbox.value);
+
+  const element = document.getElementById('display');
+  element.innerHTML = '';
+  for (var i = 0; i < store.length; i++) {
+    const para = document.createElement('p');
+    const node = document.createTextNode(store[i]);
+    para.appendChild(node);
+    element.appendChild(para);
+  }
 
   getTextbox.value = '';
+
+  console.log(store);
 }
 
 console.log('hello');
@@ -20,3 +30,13 @@ console.log('hello');
 const btn = document.getElementById('getbtn');
 
 btn.addEventListener('click', test);
+
+const getclrbtn = document.getElementById('getclrbtn');
+
+getclrbtn.addEventListener('click', getclrbtnfn);
+
+function getclrbtnfn() {
+  store = [];
+  const element = document.getElementById('display');
+  element.innerHTML = '';
+}
