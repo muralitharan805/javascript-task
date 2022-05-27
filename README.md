a9864cf1add5a760c487241854bf4108cba88407
muralitharan805 <muralitharan805@gmail.com>
2022-05-27 11:38:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-man4tu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-man4tu)
---
+++ index.js
@@ -0,0 +1,15 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+var store = '';
+
+function test() {
+  console.log('hello');
+
+  // document.getElementById('nameText').nodeValue;
+
+  // console.log(document.getElementById('nameText').nodeValue);
+}
+
+console.log('hello');
